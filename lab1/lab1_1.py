8fd723fc92a1caf97bbd84788c82ad2e57cf6a36
Sheeiavellie <josephlandmars@gmail.com>
2024-05-07 17:52:29 +0300

    fix: minor fixes

--- lab1/lab1_1.py
+++ lab1/lab1_1.py
@@ -21,7 +21,7 @@ def f(x):
     return np.cos(1.2 * x - 2) - np.cos(1.7 * x - 1) * np.sin(8.4 * x)
 
 def F(x):
-    return np.cos(1.2 * x - 2) - np.cos(1.7 * x - 1) * np.sin(8.4 * x) + 2.1337
+    return np.cos(1.2 * x - 2) - np.cos(1.7 * x - 1) * np.sin(8.4 * x) + 2
 
 def float_to_binary(number, length, min_val, max_val):
     number = max(min(number, max_val), min_val)
@@ -131,7 +131,7 @@ def genetic_algorithm(sample):
     print("----------\nGeneration #%d\nFs current: %.4f\nFs previous: %.4f\ndF: %.4f\n" % (generation_count, Fs, Fs_prev, dF))
 
     # while generation_count != 100:
-    while dF > 0.1:
+    while dF > 0.0001:
         generation_count += 1
         generation = new_generation(sample=generation_previous)
 
